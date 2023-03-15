502a8eb1a2f03f12cc566f6dd4b65baab5b1b941
unknown <aa2851214@gmail.com>
2023-03-15 16:48:33 +0700

    first commit

---
+++ Generics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Add<int>(100);
+            int result = Add1<int>(100);
+            Console.WriteLine(result);
+            Machine<int> M = new Machine<int>();
+            M.Save(10);
+            Console.WriteLine(M.Load());
+        }
+        public static void Add<T>(T a)
+        {
+            T c =  a;
+            Console.WriteLine(c);
+        }
+        static B Add1<B>(B a)
+        {
+            B c = a;
+            return c;
+        }
+    }
+    class Machine<M>
+    {
+        M saved;
+        public void Save(M item)
+        {
+            saved = item;
+        }
+        public M Load()
+        {
+            return saved;
+        }
+    }
+    
+}
